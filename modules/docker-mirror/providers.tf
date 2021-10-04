f04befc936a8bca07fb0c5f462ed4ca86f8e15b7
Eric Fritz <eric@sourcegraph.com>
2021-10-04 17:38:53 -0500

    Simplify required providers (#7)

--- modules/docker-mirror/providers.tf
+++ modules/docker-mirror/providers.tf
@@ -1,9 +1,6 @@
 terraform {
   required_version = "0.13.7"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.26"
-    }
+    google = "~> 3.26"
   }
 }
--- modules/executors/providers.tf
+++ modules/executors/providers.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = "0.13.7"
   required_providers {
-    google      = "~> 3.26"
-    google-beta = "~> 3.26"
+    google = "~> 3.26"
   }
 }

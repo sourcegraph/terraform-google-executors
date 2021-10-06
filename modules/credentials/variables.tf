1e91586de7d4f6fc380d721d23f576db1c6711c5
Erik Seliger <erikseliger@me.com>
2021-10-06 23:54:03 +0200

    Add credentials submodule (#9)
    This creates credentials for both autoscaling and metrics scraping

---
+++ modules/credentials/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type        = string
+  description = "region"
+}
+
+variable "resource_prefix" {
+  type        = string
+  description = "An optional prefix to add to all resources created."
+}

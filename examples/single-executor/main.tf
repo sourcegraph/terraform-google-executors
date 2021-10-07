c979f3763c0651c36cf9d4d6b95b0578e949e018
Erik Seliger <erikseliger@me.com>
2021-10-07 16:30:33 +0200

    Remove the EXECUTOR_FRONTEND_USERNAME variable

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -11,7 +11,6 @@ module "executors" {
   zone                                         = local.zone
   executor_instance_tag                        = "codeintel-prod"
   executor_sourcegraph_external_url            = "https://sourcegraph.acme.com"
-  executor_sourcegraph_executor_proxy_username = "executor"
   executor_sourcegraph_executor_proxy_password = "hunter2"
   executor_queue_name                          = "codeintel"
   executor_metrics_environment_label           = "prod"

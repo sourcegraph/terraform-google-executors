aaac319831396b23d22744a4fa1149386135fec3
Erik Seliger <erikseliger@me.com>
2021-10-07 16:43:12 +0200

    Revert

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -11,6 +11,7 @@ module "executors" {
   zone                                         = local.zone
   executor_instance_tag                        = "codeintel-prod"
   executor_sourcegraph_external_url            = "https://sourcegraph.acme.com"
+  executor_sourcegraph_executor_proxy_username = "executor"
   executor_sourcegraph_executor_proxy_password = "hunter2"
   executor_queue_name                          = "codeintel"
   executor_metrics_environment_label           = "prod"

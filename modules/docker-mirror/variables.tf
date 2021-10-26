662abe2c31d14b514a3a7d95e072664fe42922a8
Erik Seliger <erikseliger@me.com>
2021-10-26 21:58:01 +0200

    Use new image and expose metrics endpoint for docker registry (#14)

--- modules/docker-mirror/variables.tf
+++ modules/docker-mirror/variables.tf
@@ -1,32 +1,34 @@
 variable "zone" {
   type        = string
-  description = "zone"
+  description = "The Google zone to create the docker mirror resources in."
 }
 
 variable "network_id" {
-  type = string
+  type        = string
+  description = "The network to run the VM in."
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
+  description = "The subnet to run the VM in."
 }
 
 variable "machine_image" {
   type        = string
-  default     = "ubuntu-os-cloud/ubuntu-1804-bionic-v20200701"
-  description = "Docker registry mirror node machine disk image to use for creating the boot volume"
+  default     = "projects/sourcegraph-ci/global/images/executor-docker-mirror-98fcbdf9b8-113810"
+  description = "Docker registry mirror node machine disk image to use for creating the boot volume."
 }
 
 variable "machine_type" {
   type        = string
   default     = "n1-standard-2" // 2 vCPU, 7.5GB
-  description = "Docker registry mirror node machine type"
+  description = "Docker registry mirror node machine type."
 }
 
 variable "boot_disk_size" {
   type        = number
   default     = 64 // 64GB
-  description = "Docker registry mirror node disk size in GB"
+  description = "Docker registry mirror node disk size in GB."
 }
 
 variable "ssh_access_cidr_range" {
@@ -39,4 +41,15 @@ variable "http_access_cidr_range" {
   type        = string
   default     = "10.0.0.0/16"
   description = "CIDR range from where HTTP access to the Docker registry is acceptable."
+}
+
+variable "http_metrics_access_cidr_range" {
+  type        = string
+  default     = "0.0.0.0/0"
+  description = "CIDR range from where HTTP access to scrape metrics from the Docker registry is acceptable."
+}
+
+variable "instance_tag_prefix" {
+  type        = string
+  description = "A label tag to add to all the machines; can be used for filtering out the right instances in stackdriver monitoring and in Prometheus instance discovery."
 }

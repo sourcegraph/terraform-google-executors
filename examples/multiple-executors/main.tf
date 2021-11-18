4febc70c23df1ab864869e864d2918f7aea879a0
Eric Fritz <eric@eric-fritz.com>
2021-11-18 15:41:38 -0600

    Add providers file to keep it in-sync with AWS.

--- examples/multiple-executors/main.tf
+++ examples/multiple-executors/main.tf
@@ -1,21 +1,3 @@
-locals {
-  project = "<your GCP project name here>"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-provider "google-beta" {
-  project = local.project
-  region  = local.region
-  zone    = local.zone
-}
-
-provider "google" {
-  project = local.project
-  region  = local.region
-  zone    = local.zone
-}
-
 module "networking" {
   source  = "sourcegraph/executors/google//modules/networking"
   version = "0.0.18"

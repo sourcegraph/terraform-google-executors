b0ec08446d72f718b942771a3453b24c8bace2ba
Chris Wendt <chrismwendt@gmail.com>
2021-11-17 18:41:28 -0700

    add google and google-beta
    resolves https://github.com/sourcegraph/sourcegraph/issues/27868

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -1,6 +1,19 @@
 locals {
-  region = "us-central1"
-  zone   = "us-central1-c"
+  project = "<your GCP project name here>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+provider "google-beta" {
+  project = local.project
+  region  = local.region
+  zone    = local.zone
+}
+
+provider "google" {
+  project = local.project
+  region  = local.region
+  zone    = local.zone
 }
 
 module "executors" {

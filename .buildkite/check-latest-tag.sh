656fef37bc4268630b907f3bc9186f46f20619a6
Chris Wendt <chrismwendt@gmail.com>
2021-11-23 14:11:24 -0700

    use scratch dir in check-latest-tag.sh

--- .buildkite/check-latest-tag.sh
+++ .buildkite/check-latest-tag.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+function finish {
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
 set -ex
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
@@ -22,10 +28,10 @@ get_latest() {
 
 latest="$(get_latest)"
 
-git grep --line-number "# LATEST" examples >want.txt || true
-git grep --line-number "\"$latest\" # LATEST" examples >got.txt || true
+git grep --line-number "# LATEST" examples >"$scratch/want.txt" || true
+git grep --line-number "\"$latest\" # LATEST" examples >"$scratch/got.txt" || true
 
-if ! git diff --no-index --exit-code want.txt got.txt; then
+if ! git diff --no-index --exit-code "$scratch/want.txt" "$scratch/got.txt"; then
   echo ""
   echo "❌ Detected old versions! Make sure that all versions \`version = \"...\" # LATEST\` match the latest git tag: $latest"
   exit 1

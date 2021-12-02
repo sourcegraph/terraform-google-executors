fad8eb90a72227b20a1e8c5ae902bb4836ff47c1
Erik Seliger <erikseliger@me.com>
2021-12-02 20:23:35 +0100

    Fix image versions
    The docker registry was just a noop and the executor version was too old for the 3.34 release.

--- modules/docker-mirror/variables.tf
+++ modules/docker-mirror/variables.tf
@@ -15,7 +15,7 @@ variable "subnet_id" {
 
 variable "machine_image" {
   type        = string
-  default     = "projects/sourcegraph-ci/global/images/executor-docker-mirror-d636cd1648-113833"
+  default     = "projects/sourcegraph-ci/global/images/executor-docker-mirror-18d914ef3a-118088"
   description = "Docker registry mirror node machine disk image to use for creating the boot volume."
 }
 

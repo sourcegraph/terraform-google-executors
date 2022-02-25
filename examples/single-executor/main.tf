b92d0cf0137fad10b7f2566bd2b3848e10f7803a
Erik Seliger <erikseliger@me.com>
2022-02-25 16:09:29 +0100

    Release v3.37.2

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "3.37.1" # LATEST
+  version = "3.37.2" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

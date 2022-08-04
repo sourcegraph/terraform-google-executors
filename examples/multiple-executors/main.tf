60b31af1eacd693719d926e73f6528a313d7ffa4
Erik Seliger <erikseliger@me.com>
2022-08-04 12:07:34 +0200

    Fixup

--- examples/multiple-executors/main.tf
+++ examples/multiple-executors/main.tf
@@ -19,34 +19,34 @@ module "executors-codeintel" {
   source  = "sourcegraph/executors/google//modules/executors"
   version = "3.42.0" # LATEST
 
-  zone                                     = local.zone
-  network_id                               = module.networking.network_id
-  subnet_id                                = module.networking.subnet_id
-  resource_prefix                          = "codeintel-prod"
-  instance_tag                             = "codeintel-prod"
-  sourcegraph_external_url                 = "https://sourcegraph.acme.com"
-  sourcegraph_executor_proxy_password      = "hunter2"
-  queue_name                               = "codeintel"
-  metrics_environment_label                = "prod"
-  docker_registry_mirror                   = "http://${module.docker-mirror.ip_address}:5000"
-  docker_registry_mirror_node_exporter_url = "http://${module.docker-mirror.ip_address}:9999"
-  use_firecracker                          = true
+  zone                                = local.zone
+  network_id                          = module.networking.network_id
+  subnet_id                           = module.networking.subnet_id
+  resource_prefix                     = "codeintel-prod"
+  instance_tag                        = "codeintel-prod"
+  sourcegraph_external_url            = "https://sourcegraph.acme.com"
+  sourcegraph_executor_proxy_password = "hunter2"
+  queue_name                          = "codeintel"
+  metrics_environment_label           = "prod"
+  docker_registry_mirror              = "http://${module.docker-mirror.ip_address}:5000"
+  #   docker_registry_mirror_node_exporter_url = "http://${module.docker-mirror.ip_address}:9999"
+  use_firecracker = true
 }
 
 module "executors-batches" {
   source  = "sourcegraph/executors/google//modules/executors"
   version = "3.42.0" # LATEST
 
-  zone                                     = local.zone
-  network_id                               = module.networking.network_id
-  subnet_id                                = module.networking.subnet_id
-  resource_prefix                          = "batches-prod"
-  instance_tag                             = "batches-prod"
-  sourcegraph_external_url                 = "https://sourcegraph.acme.com"
-  sourcegraph_executor_proxy_password      = "hunter2"
-  queue_name                               = "batches"
-  metrics_environment_label                = "prod"
-  docker_registry_mirror                   = "http://${module.docker-mirror.ip_address}:5000"
-  docker_registry_mirror_node_exporter_url = "http://${module.docker-mirror.ip_address}:9999"
-  use_firecracker                          = true
+  zone                                = local.zone
+  network_id                          = module.networking.network_id
+  subnet_id                           = module.networking.subnet_id
+  resource_prefix                     = "batches-prod"
+  instance_tag                        = "batches-prod"
+  sourcegraph_external_url            = "https://sourcegraph.acme.com"
+  sourcegraph_executor_proxy_password = "hunter2"
+  queue_name                          = "batches"
+  metrics_environment_label           = "prod"
+  docker_registry_mirror              = "http://${module.docker-mirror.ip_address}:5000"
+  #   docker_registry_mirror_node_exporter_url = "http://${module.docker-mirror.ip_address}:9999"
+  use_firecracker = true
 }

2d6fd627a60a0eb072f2e89972f8e2d0c0affc03
Erik Seliger <erikseliger@me.com>
2022-09-21 08:11:59 +0200

    Prepare 4.0.0 release

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "3.43.0" # LATEST
+  version = "4.0.0" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

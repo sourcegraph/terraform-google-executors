17038965cd624390906b9bf78a85185f0e9c0a86
Dax McDonald <dax@sourcegraph.com>
2023-02-27 16:30:38 +0100

    Update files for 4.5.1 release (#88)

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "4.5.0" # LATEST
+  version = "4.5.1" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

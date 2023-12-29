969dd7d6eab17cb14769af034492607fed1e7e64
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-12-29 10:26:34 +0000

    WB-WIP: Test Release 6.6.666
    This is a Test release

--- modules/docker-mirror/main.tf
+++ modules/docker-mirror/main.tf
@@ -48,7 +48,7 @@ resource "google_compute_disk" "registry-data" {
 data "google_compute_image" "mirror_image" {
   count   = var.machine_image != "" ? 0 : 1
   project = "sourcegraph-ci"
-  family  = "sourcegraph-executors-docker-mirror-5-2"
+  family  = "sourcegraph-executors-docker-mirror-7-7"
 }
 
 resource "random_id" "compute_instance_default" {

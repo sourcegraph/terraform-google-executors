262b317b5d87beb1f3ddee3a71b01095bf97b0d4
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-01-09 09:34:18 +0000

    release-minor: v6.6.666
    {"version":"v6.6.666","inputs":"","type":"minor"}

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "5.2.0" # LATEST
+  version = "6.6.666" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

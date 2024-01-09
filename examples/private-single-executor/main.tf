c5007653dcf49ff97f661a91b6ed4c6d113231c7
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-01-09 11:13:24 +0200

    Revert "release-minor: v6.6.666"
    This reverts commit 0d9740ac16db5a368fcf86412d66bdf3ba4e152b.

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "6.6.666" # LATEST
+  version = "5.2.0" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

9d02275bc6fd58d5e191fc4766dba437e9460b32
Anish Lakhwara <anish+git@lakhwara.com>
2024-09-04 22:33:40 +0000

    release_minor: v5.7.0
    {"version":"v5.7.0","inputs":"server=v5.7.0","type":"minor"}

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "5.6.0" # LATEST
+  version = "5.7.0" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

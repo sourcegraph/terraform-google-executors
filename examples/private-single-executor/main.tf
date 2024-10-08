1d51e322c8c7e3a8e694d62a63cf600060d9358f
Jacob Pleiness <jacob.pleiness@sourcegraph.com>
2024-10-08 12:03:17 -0400

    Update files for 5.8.1 release

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "5.8.0" # LATEST
+  version = "5.8.1" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone

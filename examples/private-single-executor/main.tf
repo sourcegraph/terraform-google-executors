7ddc81c216d7672d7953c92ce7c400b874c15b3a
Warren Gifford <warrenbruceg@gmail.com>
2025-05-28 15:47:17 +0000

    release_minor: v6.4.0
    {"version":"v6.4.0","inputs":"server=v6.4.0","type":"minor"}

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -1,6 +1,6 @@
 module "executors" {
   source  = "sourcegraph/executors/google"
-  version = "6.3.0" # LATEST
+  version = "6.4.0" # LATEST
 
   region                                       = local.region
   zone                                         = local.zone
